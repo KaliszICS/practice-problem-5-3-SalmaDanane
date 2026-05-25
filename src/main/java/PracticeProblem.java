3a96ed3343ce7b3917911c882cd0e3ccc91836cc
SalmaDanane <343249595@tdsb.ca>
2026-05-25 10:11:17 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,56 @@ public class PracticeProblem {
 
 	}
 
+	public static String[] addString(String[] arr, String s, int num) {
+		
+		String[] arr2 = new String[arr.length+1];
+		int index;
+		index = 0
+
+		for (int i = 0; i < arr2.length; i++) {
+			
+			if (i != num){
+				
+				arr2[i] = arr[index];
+				index++;
+				
+			}
+				
+			else {
+				
+				arr2[i] = s;
+			}
+		}
+		return arr2;
+	}
+
+	
+	public static char[] removeChars(char[] arr, char c) {
+		
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] == c) {
+				
+				count = count + 1;
+			}
+		}
+
+		int index;
+		index = 0;
+
+		char[] arr2 = new char[arr.length - count];
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			if (arr[i] != c) {
+				arr2[index] = arr[i];
+				index++;
+			}
+		}
+
+		return arr2;
+	}
+
 }
